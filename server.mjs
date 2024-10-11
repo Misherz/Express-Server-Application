c4f462bb66bd6c086fe9c7116e08f60ef60f91fd
Misherz <michelle.j.lugo@gmail.com>
2024-10-11 14:32:35 -0400

    Server Boilerplate done

---
+++ server.mjs
@@ -0,0 +1,15 @@
+//Import
+import express from 'express';
+
+//Create an instance/Initialize Express in a variable
+const app = express();
+const PORT = 3000;
+
+//Middleware
+
+//Route
+
+//Listening
+app.listen(PORT, ()=>{
+    console.log(`Server is running on PORT ${PORT}.`)
+})
